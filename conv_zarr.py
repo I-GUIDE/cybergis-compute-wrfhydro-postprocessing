89222b1e5810b2c4555f00e0dd1963637c10539d
jparkgeo <jparkgeo@gmail.com>
2024-08-15 09:47:40 -0400

    adding latest version of the script

--- conv_zarr.py
+++ conv_zarr.py
@@ -1,3 +1,65 @@
+import subprocess
+
+import sys
+
+
+
+# Function to install packages
+
+def install(package):
+
+    subprocess.check_call([sys.executable, "-m", "pip", "install", package])
+
+
+
+# List of required packages
+
+required_packages = [
+
+    "glob2",
+
+    "fsspec",
+
+    "ujson",
+
+    "dask",
+
+    "xarray",
+
+    "pandas",
+
+    "dataretrieval",
+
+    "s3fs",
+
+    "netCDF4",
+
+    "zarr",
+
+    "kerchunk"
+
+]
+
+
+
+# Install all required packages
+
+for package in required_packages:
+
+    try:
+
+        __import__(package)
+
+    except ImportError:
+
+        install(package)
+
+
+
+print("All required packages are installed.")
+
+
+
 #imports
 import glob
 import logging
@@ -26,41 +88,44 @@ print("imports are all successfull")
 
 # Creating a temporary json folder to store the information in netcdf files as jsons files 
 
-pathlib.Path('/job/result/jsons/').mkdir(exist_ok=True)
-pathlib.Path('/job/result/zarr_files/').mkdir(exist_ok=True)
+pathlib.Path('/anvil/scratch/x-cybergis/compute/wrfhydro_postprocessing/all_code/jsons/logan').mkdir(exist_ok=True)
+pathlib.Path('/anvil/scratch/x-cybergis/compute/wrfhydro_postprocessing/all_code/zarr_results/logan').mkdir(exist_ok=True)
 output_wrfhydro="/compute_scratch"
 
 #input year 
-#year ='2018'
-#jobid='1708555341i9ik1' 
-year = os.getenv("param_output_year")
-jobid = os.getenv("param_wrfhydro_output_path")
+year ='2012'
+jobid='1723070663hQEyr' 
+#year = os.getenv("param_output_year")
+#jobid = os.getenv("param_wrfhydro_output_path")
 
 if __name__ == '__main__':
     client = Client()
     client
-    chrtout_path = '{0}/{1}/Outputs/CHRTOUT/{2}*.CHRTOUT_DOMAIN1'.format(output_wrfhydro,jobid,year)
+    chrtout_path = '/anvil/scratch/x-cybergis/compute/1723070663hQEyr/Outputs/CHRTOUT_DOMAIN1/*'
     chrtout_path=glob.glob(chrtout_path)
     files=[]
     for folder_path in chrtout_path:
         files.append(folder_path)
+    print(files)
 
 #function to generate json files from the netcdf files
 
     def gen_json(u):
         with fsspec.open(u) as inf:
             h5chunks = SingleHdf5ToZarr(inf, u, inline_threshold=300)
-            with open(f"/job/result/jsons/{u.split('/')[-1]}.json", 'wb') as outf:
+            with open(f"/anvil/scratch/x-cybergis/compute/wrfhydro_postprocessing/all_code/jsons/logan/{u.split('/')[-1]}.json", 'wb') as outf:
                outf.write(ujson.dumps(h5chunks.translate()).encode())
 
     dask.compute(*[dask.delayed(gen_json)(u) for u in files])
 
-    json_list =sorted(glob.glob('/job/result/jsons/{0}*.json'.format(year)))
+    json_list =sorted(glob.glob('/anvil/scratch/x-cybergis/compute/wrfhydro_postprocessing/all_code/jsons/logan/{0}*.json'.format(year)))
+    
+    print(json_list)
     mzz = MultiZarrToZarr(json_list,concat_dims='time',inline_threshold=0)
-    mzz.translate('/job/result/jsons/combined.json')
+    mzz.translate('/anvil/scratch/x-cybergis/compute/wrfhydro_postprocessing/all_code/jsons/logan/combined.json')
     fs = fsspec.filesystem(
     "reference", 
-    fo="/job/result/jsons/combined.json", 
+    fo="/anvil/scratch/x-cybergis/compute/wrfhydro_postprocessing/all_code/jsons/logan/combined.json", 
     skip_instance_cache=True
 )
     m = fs.get_mapper("")
